c3d51195867a224838b0bc9f5d187bc4978eaacd
NICOLE CARVALHO SOUZA <nicole.souza28@senaisp.edu.br>
2026-06-08 11:47:22 -0300

    Exercicio 4 - conceitos iniciais JS - função

---
+++ ExemploJS/exercicio4.js
@@ -0,0 +1,20 @@
+function soma(num1, num2){
+  console.log(num1 + num2);
+};
+
+function sub(num1, num2){
+  console.log(num1 - num2);
+};
+
+function mult(num1, num2){
+  console.log(num1 * num2);
+};
+
+function div(num1, num2){
+  console.log(num1 / num2);
+};
+
+soma(25, 26);
+sub(98, 20);
+mult(45, 656);
+div(456, 456);
