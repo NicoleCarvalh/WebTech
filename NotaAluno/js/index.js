48ce12fed8a7c1e18cf02e1dd12c8ff6e6984873
NicoleCarvalh <nicolecarvalho6@gmail.com>
2026-06-10 11:39:28 -0300

    feat: adição de nome de aluno no resultado da nota

--- NotaAluno/js/index.js
+++ NotaAluno/js/index.js
@@ -1,4 +1,5 @@
 const inputNota = document.getElementById("nota");
+const inputNome = document.getElementById("nome");
 const resultado = document.getElementById("resultado");
 const message = document.getElementById("message");
 
@@ -8,15 +9,16 @@ const message = document.getElementById("message");
 function resultadoNotaClick(event){
   event.preventDefault();
 
+  
 
   if(inputNota.value == ""){
     resultado.innerHTML = `<span id="message" style="display: block;">O valor não pode ser vazio</span>`
 
   } else if (inputNota.value >= 6) {
-    resultado.innerHTML = `<span id="message" style="display: block; color: #42f545;">Aluno aprovado</span>`
+    resultado.innerHTML = `<span id="message" style="display: block; color: #42f545;">${inputNome.value} foi aprovado</span>`
     
   } else {
-    resultado.innerHTML = `<span id="message" style="display: block; color: #f54242;">Aluno reprovado</span>`
+    resultado.innerHTML = `<span id="message" style="display: block; color: #ff0909; background-color: #fff;">${inputNome.value} foi reprovado</span>`
 
   }
 
