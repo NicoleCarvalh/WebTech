4bf896581566c57d91fd657910dc54cc7f368b74
NicoleCarvalh <nicolecarvalho6@gmail.com>
2026-08-12 10:18:51 -0300

    feat: Primeiro projeto React do semestre

---
+++ introducao-react/src/App.jsx
@@ -0,0 +1,37 @@
+import Header from './components/Header.jsx'
+
+function App() {
+  return (
+    <>
+      <Header/>
+
+      <section>
+        <article>
+          <h1>Lorem ipsum dolor, sit amet consectetur</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo
+            fugiat suscipit obcaecati ex ratione quos est, minus placeat? Esse
+            sint, nobis dignissimos molestias excepturi vitae minus vel non
+            nostrum!
+          </p>
+        </article>
+        <article>
+          <h1>Lorem ipsum dolor, sit amet consectetur</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quo
+            fugiat suscipit obcaecati ex ratione quos est, minus placeat? Esse
+            sint, nobis dignissimos molestias excepturi vitae minus vel non
+            nostrum!
+          </p>
+        </article>
+        <hr />
+      </section>
+
+      <footer>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
